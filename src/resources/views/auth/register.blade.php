dd8e96369ae78d5802ad3d778137e8defde46e95
takeda shigeki <takeda.shigeki0614@gmail.com>
2024-02-12 16:54:47 +0900

    ファイルの追加

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -7,14 +7,14 @@
 @section('content')
 <div class="register__content">
   <div class="register-form__heading">
-    <h2>会員登録</h2>
+    <h2>Registration</h2>
   </div>
   <form class="form" action="/register" method="post">
     @csrf
     <div class="form__group">
       <div class="form__group-content">
         <div class="form__input--text">
-          <input type="text" name="name" placeholder="名前" value="{{ old('name') }}" />
+          <input type="text" name="name" placeholder="Username" value="{{ old('name') }}" />
         </div>
         <div class="form__error">
           @error('name')
@@ -26,7 +26,7 @@
     <div class="form__group">
       <div class="form__group-content">
         <div class="form__input--text">
-          <input type="email" name="email" placeholder="メールアドレス" value="{{ old('email') }}" />
+          <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" />
         </div>
         <div class="form__error">
           @error('email')
@@ -38,7 +38,7 @@
     <div class="form__group">
       <div class="form__group-content">
         <div class="form__input--text">
-          <input type="password" name="password" placeholder="パスワード" />
+          <input type="password" name="password" placeholder="Password" />
         </div>
         <div class="form__error">
           @error('password')
@@ -47,15 +47,8 @@
         </div>
       </div>
     </div>
-    <div class="form__group">
-      <div class="form__group-content">
-        <div class="form__input--text">
-          <input type="password" name="password_confirmation" placeholder="確認用パスワード" />
-        </div>
-      </div>
-    </div>
     <div class="form__button">
-      <button class="form__button-submit" type="submit">会員登録</button>
+      <button class="form__button-submit" type="submit">登録</button>
     </div>
   </form>
   <div class="login__link">
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/', [AuthController::class, 'index']);
 });
