14cc1c5a537b75de76eb023030fd0d0172e12c11
takeda shigeki <takeda.shigeki0614@gmail.com>
2024-03-19 16:50:35 +0900

    ファイルの追加

--- src/resources/views/status.blade.php
+++ src/resources/views/status.blade.php
@@ -1,59 +1,77 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/my_page.css') }}">
+<link rel="stylesheet" href="{{ asset('css/status.css') }}">
 @endsection
 
 @section('content')
 <div class="user">
-  <p class="user__name">
-    <?php $user = Auth::user(); ?>{{ $user->name }}さん
-  </p>
+    <p class="user__name">
+        <?php $user = Auth::user(); ?>{{ $user->name }}さん
+    </p>
 </div>
 
-<div class="attendance__content">
-  <div class="attendance__panel">
-    <div>
-      予約状況
-      @foreach ($my_bookings as $my_booking)
-      <div>
-        Shop {{ $my_booking->restaurant->name }}
-        Date {{ date('Y-m-d', strtotime($my_booking->booking_time)) }}
-        Time {{ date('H:i', strtotime($my_booking->booking_time)) }}
-        Number {{ $my_booking->number }}人
-        <form class="delete-form" action="/my_page/status" method="post">
-          @method('DELETE')
-          @csrf
-          <div class="delete-form__button">
-            <input type="hidden" name="my_booking_id" value="{{ $my_booking['id'] }}">
-            <button class="delete-form__button-submit" type="submit">予約取り消し</button>
-            {{ $message ?? '' }}
-          </div>
-        </form>
-      </div>
-      @endforeach
+<div class="title">
+    <p class="title__booking">予約状況</p>
+    <p class="title__favorite">お気に入り店舗</p>
+</div>
+
+<div class="status">
+    <div class="status__booking">
+        @foreach ($my_bookings as $my_booking)
+        <div class="status__booking-each">
+            <table>
+                <tr>
+                    <td>Shop:</td><td>{{ $my_booking->restaurant->name }}</td>
+                </tr>
+                <tr>
+                    <td>Date:</td><td>{{ date('Y-m-d', strtotime($my_booking->booking_time)) }}</td>
+                </tr>
+                <tr>
+                    <td>Time:</td><td>{{ date('H:i', strtotime($my_booking->booking_time)) }}</td>
+                </tr>
+                <tr>
+                    <td>Number:</td><td>{{ $my_booking->number }}人</td>
+                </tr>
+            </table>
+            <form action="/my_page/status" method="post">
+                @method('DELETE')
+                @csrf
+                <div class="status__booking-each-delete">
+                    <input type="hidden" name="my_booking_id" value="{{ $my_booking['id'] }}">
+                    <button class="status__booking-each-delete-button" type="submit">予約取り消し</button>
+                    {{ $message ?? '' }}
+                </div>
+            </form>
+        </div>
+        @endforeach
     </div>
-    <div class="attendance__button">
-      お気に入り店舗
-      @foreach ($my_favorites as $my_favorite)
-      <div>
-        <img src="/images/{{ $my_favorite->restaurant_id }}.jpg" width=20%>
-        {{ $my_favorite->restaurant->name }}
-        ＃{{ $my_favorite->restaurant->prefecture }}　＃{{ $my_favorite->restaurant->genre }}
-        <form action="/shop_detail" method="get">
-          @csrf
-          <input type="hidden" value="{{$my_favorite->restaurant->id}}" name="restaurant_id">
-          <button class="restaurant__detail-submit" type="submit">詳しく見る</button>
-        </form>
-        <form action="/my_page/status" method="post">
-          @csrf
-          <input type="hidden" name="favorite" value="{{$my_favorite->restaurant->id}}">
-          <button type="submit">@if($my_favorites->contains('restaurant_id', $my_favorite->restaurant->id)){{ '♥' }}@else{{ '♡' }}@endif</button>
-        </form>
-      </div>
-      @endforeach
+
+    <div class="status__favorite">
+        @foreach ($my_favorites as $my_favorite)
+        <div class="status__favorite-each">
+            <img class="restaurant__favorite-each-image" src="/images/{{ $my_favorite->restaurant_id }}.jpg" width=20%>
+            <div class="restaurant__favorite-each-explanation">
+                <br>
+                <h class="restaurant__favorite-each-name">&emsp;{{$my_favorite->restaurant->name }}</h><br>
+                <small>&emsp;＃{{ $my_favorite->restaurant->prefecture }}&ensp;＃{{ $my_favorite->restaurant->genre }}</small>
+                <div class="restaurant__favorite-each-form">
+                    <form action="/shop_detail" method="get">
+                    @csrf
+                        <input type="hidden" value="{{$my_favorite->restaurant->id}}" name="restaurant_id">
+                        <button class="restaurant__favorite-each--detail" type="submit">詳しく見る</button>
+                        <p></p>
+                    </form>
+                    <form action="/my_page/status" method="post">
+                    @csrf
+                        <input type="hidden" name="favorite" value="{{$my_favorite->restaurant->id}}">
+                        <span style="color:#FF00CC">&#x2764;</span>
+                    </form>
+                </div>
+            </div>
+        </div>
+        @endforeach
     </div>
-  </div>
 </div>
 
 @endsection
